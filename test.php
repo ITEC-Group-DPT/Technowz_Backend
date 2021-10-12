03db7b68f74e7bf8eb8d5a840bd01e9c33501bac
chanhbeck <chanhbeck27@gmail.com>
2021-10-12 17:53:34 +0700

    fcku

--- test.php
+++ test.php
@@ -2,17 +2,15 @@
 header("Access-Control-Allow-Origin: *");
 
 if (isset($_SERVER["HTTP_ORIGIN"])) {
-    echo json_encode($_SERVER["HTTP_ORIGIN"]);
+    ;
     $allowedOrigins = [
         "http://localhost:3000/",
         "http://192.168.1.155:3000/",
         // ... etc
     ];
 
-    if (in_array($_SERVER["HTTP_ORIGIN"], $allowedOrigins)) {
-        echo "?????";
-        header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
-    }
+    header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+    
     // header("Access-Control-Allow-Methods: POST, GET, DELETE");
     // header("Access-Control-Allow-Headers: origin, content-type, accept");
 }
