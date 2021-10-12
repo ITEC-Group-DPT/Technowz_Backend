1600e43efe6b883ad65e100aecbd5250a08d98dc
chanhbeck <chanhbeck27@gmail.com>
2021-10-12 18:32:32 +0700

    23523

--- test.php
+++ test.php
@@ -7,22 +7,13 @@ if (isset($_SERVER["HTTP_ORIGIN"])) {
         "http://192.168.1.155:3000"
     ];
 
-    // foreach ($allowedOrigins as $origin) {
-    //     if ($origin === $_SERVER["HTTP_ORIGIN"]) {
-    //         echo "1";
-    //         header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
-            
-    //         echo "2";
-    //         break;
-    //     }
-    // }
     if (in_array($_SERVER["HTTP_ORIGIN"], $allowedOrigins)) {
         echo "?????";
         header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
     }
-    // header("Access-Control-Allow-Methods: POST, GET, DELETE");
-    // header("Access-Control-Allow-Headers: origin, content-type, accept");
+    header("Access-Control-Allow-Methods: POST, GET, DELETE");
+    header("Access-Control-Allow-Headers: origin, content-type, accept");
     
 }
 
-// echo "cho Tri";
+echo json_encode($_GET);
