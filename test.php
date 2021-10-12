a7bb2d0b5f3345500c6d9bac3566c1132a911449
chanhbeck <chanhbeck27@gmail.com>
2021-10-12 17:45:47 +0700

    asdadsasdada

--- test.php
+++ test.php
@@ -1,16 +1,17 @@
 <?php
 if (isset($_SERVER["HTTP_ORIGIN"])) {
-    $allowedOrigins = [
-        "http://localhost:3000/",
-        "http://192.168.1.155:3000/",
-        // ... etc
-    ];
+    var_dump($_SERVER["HTTP_ORIGIN"]);
+    // $allowedOrigins = [
+    //     "http://localhost:3000/",
+    //     "http://192.168.1.155:3000/",
+    //     // ... etc
+    // ];
 
-    if (in_array($_SERVER["HTTP_ORIGIN"], $allowedOrigins)) {
-        header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
-    }
-    header("Access-Control-Allow-Methods: POST, GET, DELETE");
-    header("Access-Control-Allow-Headers: origin, content-type, accept");
+    // if (in_array($_SERVER["HTTP_ORIGIN"], $allowedOrigins)) {
+    //     header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+    // }
+    // header("Access-Control-Allow-Methods: POST, GET, DELETE");
+    // header("Access-Control-Allow-Headers: origin, content-type, accept");
 }
 
 echo "cho Tri";
