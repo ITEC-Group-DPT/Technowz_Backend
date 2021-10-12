61a3bb641df839e2307bf224a4e7c6367387c1e4
chanhbeck <chanhbeck27@gmail.com>
2021-10-12 18:30:36 +0700

    312131231

--- test.php
+++ test.php
@@ -2,25 +2,24 @@
 // header("Access-Control-Allow-Origin: *");
 if (isset($_SERVER["HTTP_ORIGIN"])) {
 
-    echo "Hello " . (strcmp($_SERVER["HTTP_ORIGIN"],"http://localhost:3000"));
     $allowedOrigins = [
         "http://localhost:3000",
         "http://192.168.1.155:3000"
     ];
 
-    foreach ($allowedOrigins as $origin) {
-        if ($origin === $_SERVER["HTTP_ORIGIN"]) {
-            echo "1";
-            header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+    // foreach ($allowedOrigins as $origin) {
+    //     if ($origin === $_SERVER["HTTP_ORIGIN"]) {
+    //         echo "1";
+    //         header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
             
-            echo "2";
-            break;
-        }
-    }
-    // if (in_array($_SERVER["HTTP_ORIGIN"], $allowedOrigins)) {
-    //     echo "?????";
-    //     // header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+    //         echo "2";
+    //         break;
+    //     }
     // }
+    if (in_array($_SERVER["HTTP_ORIGIN"], $allowedOrigins)) {
+        echo "?????";
+        header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+    }
     // header("Access-Control-Allow-Methods: POST, GET, DELETE");
     // header("Access-Control-Allow-Headers: origin, content-type, accept");
     
