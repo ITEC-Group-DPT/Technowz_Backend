c917e2d75575f5c166b87cc37178adac14f12615
chanhbeck <chanhbeck27@gmail.com>
2021-10-12 22:20:03 +0700

    235

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+// header("Access-Control-Allow-Origin: *");
+if (isset($_SERVER["HTTP_ORIGIN"])) {
+
+    $allowedOrigins = [
+        "http://localhost:3000",
+        "http://192.168.1.155:3000"
+    ];
+
+    if (in_array($_SERVER["HTTP_ORIGIN"], $allowedOrigins)) {
+        header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+    }
+    header("Access-Control-Allow-Methods: POST, GET, DELETE");
+    header("Access-Control-Allow-Headers: origin, content-type, accept, userID");
+    
+}
+
+echo json_encode(getallheaders());
