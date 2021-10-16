65a43331a012529adcb355082b1cfb2f3eda7e2f
chanhbeck <chanhbeck27@gmail.com>
2021-10-16 23:40:11 +0700

    eheheh

--- api/deliAPI.php
+++ api/deliAPI.php
@@ -7,22 +7,26 @@
 
     if ($userID == NULL) errorAPI();
     else{ 
-        $deli = new DeliveryInfo($conn, $userID); 
-        if ($_GET['command']== 'getDelivery'){
-            $array = $deli->getDeliveryInfo();
-            echo json_encode($array);
+        $deli = new DeliveryInfo($conn, $userID);
+        if(isset($_POST['command'])){
+            if ($_POST['command']== 'update'){
+                $deli->updateDeliveryInfo($_POST['deliID'], $_POST['name'], $_POST['address'], $_POST['phone']);
+                echo "updated";
+            }
+            else if ($_POST['command']== 'create'){
+                $id = $deli->createDeliveryInfo($_POST['name'], $_POST['address'], $_POST['phone']);
+                echo "created";
+            }
+            else if ($_POST['command']== 'delete'){
+                $deli->deleteDelivery($_POST['deliID']);
+                echo "deleted";
+            }
         }
-        elseif ($_POST['command']== 'update'){
-            $deli->updateDeliveryInfo($_POST['deliID'], $_POST['name'], $_POST['address'], $_POST['phone']);
-            echo "updated";
-        }
-        elseif ($_POST['command']== 'create'){
-            $id = $deli->createDeliveryInfo($_POST['name'], $_POST['address'], $_POST['phone']);
-            echo "created";
-        }
-        elseif ($_POST['command']== 'delete'){
-            $deli->deleteDelivery($_POST['deliID']);
-            echo "deleted";
+        else if(isset($_GET['command'])){
+            if($_GET['command']== 'getDelivery'){
+                $array = $deli->getDeliveryInfo();
+                echo json_encode($array);
+            }
         }
     }
 ?>
--- api/orderAPI.php
+++ api/orderAPI.php
@@ -1,26 +1,30 @@
 <?php 
     include './apiheader.php';
-    include '../classed/Order.php';
+    include '../classes/Order.php';
 
     $header = getallheaders();
     $userID = $header['Userid'];
-
+    
     if($userID == NULL) errorAPI();
     else{
         $order = new Order($conn, $userID);
-        if($_POST['command'] == 'createOrder'){
-            $itemList = json_decode($_POST['itemList']);
-            $order->createOrder($_POST['name'], $_POST['address'], $_POST['phone'], $_POST['totalPrice'], $itemList);
-            echo "created";
+        if(isset($_POST['command'])){
+            if($_POST['command'] == 'createOrder'){
+                $itemList = json_decode($_POST['itemList']);
+                $order->createOrder($_POST['name'], $_POST['address'], $_POST['phone'], $_POST['totalPrice'], $itemList);
+                echo "created";
+            }
         }
-        else if($_GET['command'] == 'getOrderDetail'){
-            $arr = [];
-            $arr['orderInfo'] = $order->getOrderInfo($_GET['orderID']);
-            $arr['itemList'] =  $order->getItemList($_GET['orderID']);
-            echo json_encode($arr);
-        }
-        else if($_GET['command'] == 'getOrderList'){
-            echo json_encode($order->getOrderList());
+        else if(isset($_GET['command'])){
+            if($_GET['command'] == 'getOrderDetail'){
+                $arr = [];
+                $arr['orderInfo'] = $order->getOrderInfo($_GET['orderID']);
+                $arr['itemList'] =  $order->getItemList($_GET['orderID']);
+                echo json_encode($arr);
+            }
+            else if($_GET['command'] == 'getOrderList'){
+                echo json_encode($order->getOrderList());
+            }
         }
     }
 ?>
