51070c99db0c83810c53b4f43df6cd6685dcc799
JustTory <johnnynathany@gmail.com>
2021-10-25 15:42:14 +0700

    change localhost db

--- database/db.php
+++ database/db.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "containers-us-west-19.railway.app";
+    $host = "localhost";
     $user = "root";
-    $pw = "8Rw056G8lsxkT8VGTE9h";
-    $db = "railway";
-    $port = "6789";
+    $pw = "";
+    $db = "technow";
+    $port = "3306";
     $conn = new mysqli($host, $user, $pw, $db, $port);
 ?>
