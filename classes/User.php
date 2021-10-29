c9d02e840842a106f00b1a6bd8ff562ec9ab10aa
JustTory <johnnynathany@gmail.com>
2021-10-29 19:06:04 +0700

    error msg changed

--- classes/User.php
+++ classes/User.php
@@ -34,7 +34,7 @@
                 }
                 else $res['data']['errorPassword'] = "Password is incorrect";
             }
-            else $res['data']['errorEmail'] = "Email does not exist";
+            else $res['data']['errorEmail'] = "Email not found";
             return $res;
         }
 
