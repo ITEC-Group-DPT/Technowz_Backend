45224049a558721086ea0f6ca7441fa1316d9dd5
JaeySen <longhientranjjj@gmail.com>
2021-10-29 11:33:39 +0700

    admin APIs
    missing: img234 optional, img db

--- api/productAPI.php
+++ api/productAPI.php
@@ -1,42 +1,80 @@
-<?php 
+<?php
 	include './apiheader.php';
 	include '../classes/Product.php';
     include '../classes/Favorite.php';
 
-    if(isset($_GET['command'])){
-        $productID = (isset($_GET['productID'])) ? $_GET['productID'] : '';
-        if($_GET['command'] == 'getProduct'){
-            $header = getallheaders();
+    $header = getallheaders();
+    if(isset($header['userid'])){
+        $userID = $header['userid'];
+        if(isset($_GET['command'])){
+            $productID = (isset($_GET['productID'])) ? $_GET['productID'] : '';
+            if($_GET['command'] == 'getProduct'){
+                // $header = getallheaders();
+                $product = new Product($conn, $productID);
+                $arr = [];
+                $arr['product'] = $product->getProduct();
+    
+                if(isset($header['Userid'])){
+                    $favorite = new Favorite($conn, $header['Userid']);
+                    $arr['favorite'] = $favorite->checkFavorite($productID);
+                }
+                else $arr['favorite'] = false;
+    
+                if($arr['product'] != [])
+                    successApi($arr);
+                else failAPI("No product found");
+            }
+            else if($_GET['command'] == 'getProductCategory'){
+                $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 20;
+                $data = Product::getProductsByCategory($conn, $_GET['typeOfProduct'], $limit);
+                successApi($data);
+            }
+            else if($_GET['command'] == 'getTopRating'){
+                $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 20;
+                $data = Product::getTopRating($conn, $limit);
+                successApi($data);
+            }
+            else if($_GET['command'] == 'searchProducts'){
+                $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 5;
+                $data = Product::getProducts($conn, $_GET['searchValue'], $limit);
+                successApi($data);
+            }
+            else failApi("No command found!");
+        }
+        else if(isset($_POST['command'])){
+            $productID = (isset($_POST['productID'])) ? $_POST['productID'] : '';
             $product = new Product($conn, $productID);
-            $arr = [];
-            $arr['product'] = $product->getProduct();
-
-            if(isset($header['userid'])){
-                $favorite = new Favorite($conn, $header['userid']);
-                $arr['favorite'] = $favorite->checkFavorite($productID);
-            } 
-            else $arr['favorite'] = false;
-
-            if($arr['product'] != [])
-                successApi($arr);
-            else failAPI("No product found");
-        }
-        else if($_GET['command'] == 'getProductCategory'){
-            $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 20;
-            $data = Product::getProductsByCategory($conn, $_GET['typeOfProduct'], $limit);
-            successApi($data);
-        }
-        else if($_GET['command'] == 'getTopRating'){
-            $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 20;
-            $data = Product::getTopRating($conn, $limit);
-            successApi($data);
-        }
-        else if($_GET['command'] == 'searchProducts'){
-            $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 5;
-            $data = Product::getProducts($conn, $_GET['searchValue'], $limit);
-            successApi($data);
+            $data = [];
+            $data['type'] = $_POST['type'];
+            $data['description'] = $_POST['description'];
+            $data['spec'] = $_POST['spec'];
+            $data['name'] = $_POST['name'];
+            $data['price'] = $_POST['price'];
+            $data['rating'] = $_POST['rating'];
+            $data['sold'] = $_POST['sold'];
+            $data['img1'] = $_POST['img1'];
+            $data['img2'] = $_POST['img2'];
+            $data['img3'] = $_POST['img3'];
+            $data['img4'] = $_POST['img4'];
+            
+            if($_POST['command'] == 'create'){
+				if($product->createProduct($data))
+					successApi("New product created");
+				else failApi("No product created");
+			} 
+            else if($_POST['command'] == 'modify'){
+				if($product->modifyProduct($data))
+					successApi("A product is modified");
+				else failApi("No product is modified");
+			} 
+			else if($_POST['command'] == 'remove'){
+				if($product->removeProduct())
+					successApi("A product is removed");
+				else failApi("No product is removed");
+			} 
+			else failApi('No command found');
         }
         else failApi("No command found!");
-	}
-    else failApi("No command found!");
+    }
+    
 ?>
--- classes/Product.php
+++ classes/Product.php
@@ -20,7 +20,7 @@
         }
 
         //static
-        public static function getProductsByCategory($conn, $type, $limit = 20, $offset = 0){
+        public static function getProductsByCategory($conn, $type, $limit = 10, $offset = 0){
             $stmt = $conn->prepare("SELECT p.productID, p.name, pimg.img1, p.rating, p.sold, p.price
                                     from products p, productimage pimg
                                     where p.type = ? and p.productID = pimg.productID limit ?,?");
@@ -30,7 +30,7 @@
             return $results->fetch_all(MYSQLI_ASSOC);
         }
 
-        public static function getTopRating($conn, $limit = 20){
+        public static function getTopRating($conn, $limit = 10){
             $stmt = $conn->prepare("SELECT p.productID, p.name, pimg.img1, p.rating, p.sold, p.price
                                     from products p, productimage pimg 
                                     where p.productID = pimg.productID 
@@ -50,6 +50,44 @@
             $stmt->execute();
             $results = $stmt->get_result();
             return $results->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function createProduct($data){
+            $stmt1 = $this->conn->prepare("INSERT INTO products (type, description, spec, name, price, rating, sold) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $stmt1->bind_param("ssssidi", $data['type'], $data['description'], $data['spec'], $data['name'], $data['price'], $data['rating'], $data['sold']);
+            $stmt1->execute();
+            $this->productID = $stmt1->insert_id;
+
+            $stmt2 = $this->conn->prepare("INSERT INTO productimage (productID, img1, img2, img3, img4) VALUES (?, ?, ?, ?, ?)");
+            $stmt2->bind_param("issss", $this->productID, $data['img1'], $data['img2'], $data['img3'], $data['img4']);
+            $stmt2->execute();
+            if ($stmt1->affected_rows == 1 && $stmt2->affected_rows == 1) return $stmt1->insert_id;
+            // if ($stmt1->affected_rows == 1) return $stmt1->insert_id;
+            else return false;
+        }
+
+        public function modifyProduct($data){
+            $stmt1 = $this->conn->prepare("UPDATE products SET type = ?, description = ?, spec = ?, name = ?, price = ?, rating = ?, sold = ? WHERE productID = ?");
+            $stmt1->bind_param("ssssidii", $data['type'], $data['description'], $data['spec'], $data['name'], $data['price'], $data['rating'], $data['sold'], $this->productID);
+            $stmt1->execute();
+
+            $stmt2 = $this->conn->prepare("UPDATE productimage SET img1 = ?, img2 = ?, img3 = ?, img4 = ? WHERE productID = ?");
+            $stmt2->bind_param("ssssi", $data['img1'], $data['img2'], $data['img3'], $data['img4'], $this->productID);
+            $stmt2->execute();
+            if ($stmt1->affected_rows > 0 || $stmt2->affected_rows > 0) return true;
+            else return false;
+        }
+
+        public function removeProduct(){
+            $stmt1 = $this->conn->prepare("DELETE FROM products WHERE productID like ?");
+            $stmt1->bind_param("i", $this->productID);
+            $stmt1->execute();
+
+            $stmt2 = $this->conn->prepare("DELETE FROM productimage WHERE productID like ?");
+            $stmt2->bind_param("i", $this->productID);
+            $stmt2->execute();
+            if ($stmt1->affected_rows != 0) return true;
+            else return false;
         }
     }
 ?>
