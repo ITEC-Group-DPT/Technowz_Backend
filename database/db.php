bd559df91b7c4ea8df9804457fa5de1d1de0f025
kurozemi <tranngminhdao@gmail.com>
2021-11-02 21:12:27 +0700

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "localhost";
+    $host = "containers-us-west-19.railway.app";
     $user = "root";
-    $pw = "";
-    $db = "technow";
-    $port = "3306";
+    $pw = "8Rw056G8lsxkT8VGTE9h";
+    $db = "railway";
+    $port = "6789";
     $conn = new mysqli($host, $user, $pw, $db, $port);
 ?>
