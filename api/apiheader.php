35a43688bb73eb37cb1eac0d5a86f11bf6fb2410
kurozemi <tranngminhdao@gmail.com>
2021-11-02 23:33:42 +0700

    test FE host

--- api/apiheader.php
+++ api/apiheader.php
@@ -4,7 +4,8 @@
         $allowedOrigins = [
             "http://localhost:3000",
             "http://192.168.1.155:3000",
-            "http://localhost:8080"
+            "http://localhost:8080",
+            "https://technow-b54b6.web.app/",
         ];
 
         if (in_array($_SERVER["HTTP_ORIGIN"], $allowedOrigins)) {
