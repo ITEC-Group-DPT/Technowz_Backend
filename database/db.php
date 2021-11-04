4d24470c0566dd1ed3f9da8cad26bf5b04e3cf9d
Phu Truong <59415928+tezukashuko@users.noreply.github.com>
2021-11-04 21:15:31 +0700

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "localhost";
+    $host = "containers-us-west-15.railway.app";
     $user = "root";
-    $pw = "";
-    $db = "technow";
-    $port = "3306";
+    $pw = "F6lWlpFzjM1uxb1k9XKn";
+    $db = "railway";
+    $port = "6377";
     $conn = new mysqli($host, $user, $pw, $db, $port);
 ?>
