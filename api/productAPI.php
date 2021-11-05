cc515c92f536d8c75ebe827ca02944a28ed527b2
Miaozxje <chanhbeck27@gmail.com>
2021-11-05 16:54:01 +0700

    add updateProductRating & format

--- api/productAPI.php
+++ api/productAPI.php
@@ -46,14 +46,14 @@
         $productID = (isset($_POST['productID'])) ? $_POST['productID'] : '';
         $product = new Product($conn, $productID);
         $data = [];
-        $data['type'] = $_POST['type'];
-        $data['description'] = $_POST['description'];
-        $data['spec'] = $_POST['spec'];
-        $data['name'] = $_POST['name'];
-        $data['price'] = $_POST['price'];
-        $data['rating'] = $_POST['rating'];
-        $data['sold'] = $_POST['sold'];
-        $data['img1'] = $_POST['img1'];
+        $data['type'] = isset($_POST['type']) ? $_POST['type'] : '';
+        $data['description'] = isset($_POST['description']) ? $_POST['description'] : '';
+        $data['spec'] = isset($_POST['spec']) ? $_POST['spec'] : '';
+        $data['name'] = isset($_POST['name']) ? $_POST['name'] : '';
+        $data['price'] = isset($_POST['price']) ? $_POST['price'] : '';
+        $data['rating'] = isset($_POST['rating']) ? $_POST['rating'] : '';
+        $data['sold'] = isset($_POST['sold']) ? $_POST['sold'] : '';
+        $data['img1'] = isset($_POST['img1']) ? $_POST['img1'] : '';
         $data['img2'] = isset($_POST['img2']) ? $_POST['img2'] : "";
         $data['img3'] = isset($_POST['img3']) ? $_POST['img2'] : "";
         $data['img4'] = isset($_POST['img4']) ? $_POST['img2'] : "";
@@ -72,7 +72,12 @@
             if($product->removeProduct())
                 successApi("A product is removed");
             else failApi("No product is removed");
-        } 
+        }
+        else if($_POST['command'] == 'updateRating'){
+            if($product->updateProductRating($_POST['rating']))
+                successApi("Update product rating successfully");
+            else failApi("Can not update product rating");
+        }
         else failApi('No command found');
     }
     else failApi("No command found!");
