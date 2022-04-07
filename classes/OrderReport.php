fb40c4a2edc966919c9d7383dd1da13c39b57608
JustTory <johnnynathany@gmail.com>
2022-04-07 22:47:13 +0700

    get value of all order status (total value)

--- classes/OrderReport.php
+++ classes/OrderReport.php
@@ -120,8 +120,8 @@
                 $keyFormat = "date(now() - interval $dateBackward $sortBy)";
 
                 $sql .= "SELECT date_format($keyFormat , $format) as 'key', ifnull(sum(o.totalPrice), 0) as 'income'
-                         FROM orders o, orderstatus s
-                         WHERE o.orderID = s.orderID and s.statusID = 4 and   ";
+                         FROM orders o
+                         WHERE   ";
 
                 if ($sortBy == 'Day')
                     $sql .= "date(o.dateCreated) = date(now() - interval $dateBackward Day)";
