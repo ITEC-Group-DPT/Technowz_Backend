5148943d6f8e5afe2e827d98631e2bf576f42e02
Cuong Ly <cuong.ly@saigonmio.com>
2022-04-08 12:39:38 +0700

    done

--- classes/ProductStatistic.php
+++ classes/ProductStatistic.php
@@ -7,42 +7,49 @@
             $this->conn = $conn;
         }
 
-        public function getTotalNumberOfProductAdmin(){
-            $stmt = $this->conn->prepare("SELECT DISTINCT COUNT(p.productID)
-                                    from products p");
+        public function getTotalNumberOfProductAdmin($value){
+            $stmt = $this->conn->prepare("SELECT DISTINCT COUNT(p.productID) as totalPage
+                                        from products p
+                                        where p.name like ?");
+            $stmt->bind_param("s", $value);
+			$stmt->execute();
+			$results = $stmt->get_result();
+			return $results->fetch_all(MYSQLI_ASSOC);
+        }
+    
+        public function getNumberOfProductByCategoryAdmin($type, $value){
+            $stmt = $this->conn->prepare("SELECT DISTINCT COUNT(p.productID) as totalPage
+                                        from products p
+                                        where p.type like ? and p.name like ?");
+            $stmt->bind_param("ss", $type, $value);
             $stmt->execute();
             $results = $stmt->get_result();
             return $results->fetch_all(MYSQLI_ASSOC);
         }
     
-        public function getNumberOfProductByCategoryAdmin($type){
-            $stmt = $this->conn->prepare("SELECT DISTINCT COUNT(p.productID)
-                                    from products p
-                                    where p.type = ?");
-            $stmt->bind_param("s", $type);
-            $stmt->execute();
-            $results = $stmt->get_result();
-            return $results->fetch_all(MYSQLI_ASSOC);
-        }
-    
-        public function getAllProductByPageAdmin($offset, $limit = 6){
+        public function getAllProductByPageAdmin($offset, $limit = 6, $value, $orderBy, $order){
+			$temp = "p." . $orderBy;
             $stmt = $this->conn->prepare("SELECT *
-                                from products p, productimage img
-                                where p.productID = img.productID
-                                limit ?, ?");
-            $stmt->bind_param("ii", $offset, $limit);
+                                        from products p, productimage img
+                                        where p.productID = img.productID and p.name like ?
+                                        order by $temp $order
+                                        limit ?, ?");
+            $stmt->bind_param("sii", $value, $offset, $limit);
             $stmt->execute();
             $results = $stmt->get_result();
             return $results->fetch_all(MYSQLI_ASSOC);
         }
     
-        public function getProductByCategoryAdmin($type, $offset, $limit = 6)
+        public function getProductByCategoryAdmin($type, $offset, $limit = 6, $value, $orderBy, $order)
         {
+			$temp = "p." . $orderBy;
             $stmt = $this->conn->prepare("SELECT *
-                                    from products p, productimage pimg
-                                    where p.type = ? and p.productID = pimg.productID
-                                    limit ?, ?");
-            $stmt->bind_param("sii", $type, $offset, $limit);
+                                        from products p, productimage pimg
+                                        where p.type like ? and p.productID = pimg.productID and p.name like ?
+                                        order by $temp $order
+                                        limit ?, ?");
+            $temp = 'p.' . $orderBy;
+            $stmt->bind_param("ssii", $type, $value, $offset, $limit);
             $stmt->execute();
             $results = $stmt->get_result();
             return $results->fetch_all(MYSQLI_ASSOC);
