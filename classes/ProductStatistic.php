704001950b0b38a4a4feff8cf541a6900dc91425
Cuong Ly <cuong.ly@saigonmio.com>
2022-04-09 15:02:34 +0700

    Update ProductStatistic.php

--- classes/ProductStatistic.php
+++ classes/ProductStatistic.php
@@ -10,8 +10,8 @@
         public function getTotalNumberOfProductAdmin($value){
             $stmt = $this->conn->prepare("SELECT DISTINCT COUNT(p.productID) as totalPage
                                         from products p
-                                        where p.name like ?");
-            $stmt->bind_param("s", $value);
+                                        where p.name like ? or p.productID like ?");
+            $stmt->bind_param("ss", $value, $value);
 			$stmt->execute();
 			$results = $stmt->get_result();
 			return $results->fetch_all(MYSQLI_ASSOC);
@@ -20,8 +20,8 @@
         public function getNumberOfProductByCategoryAdmin($type, $value){
             $stmt = $this->conn->prepare("SELECT DISTINCT COUNT(p.productID) as totalPage
                                         from products p
-                                        where p.type like ? and p.name like ?");
-            $stmt->bind_param("ss", $type, $value);
+                                        where p.type like ? and (p.name like ? or p.productID like ?)");
+            $stmt->bind_param("sss", $type, $value, $value);
             $stmt->execute();
             $results = $stmt->get_result();
             return $results->fetch_all(MYSQLI_ASSOC);
@@ -31,10 +31,10 @@
 			$temp = "p." . $orderBy;
             $stmt = $this->conn->prepare("SELECT *
                                         from products p, productimage img
-                                        where p.productID = img.productID and p.name like ?
+                                        where p.productID = img.productID and (p.name like ? or p.productID like ?)
                                         order by $temp $order
                                         limit ?, ?");
-            $stmt->bind_param("sii", $value, $offset, $limit);
+            $stmt->bind_param("ssii", $value, $value, $offset, $limit);
             $stmt->execute();
             $results = $stmt->get_result();
             return $results->fetch_all(MYSQLI_ASSOC);
@@ -45,11 +45,11 @@
 			$temp = "p." . $orderBy;
             $stmt = $this->conn->prepare("SELECT *
                                         from products p, productimage pimg
-                                        where p.type like ? and p.productID = pimg.productID and p.name like ?
+                                        where p.type like ? and p.productID = pimg.productID and (p.name like ? or or p.productID like ?)
                                         order by $temp $order
                                         limit ?, ?");
             $temp = 'p.' . $orderBy;
-            $stmt->bind_param("ssii", $type, $value, $offset, $limit);
+            $stmt->bind_param("sssii", $type, $value, $value, $offset, $limit);
             $stmt->execute();
             $results = $stmt->get_result();
             return $results->fetch_all(MYSQLI_ASSOC);
